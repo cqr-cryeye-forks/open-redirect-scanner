3cad50f4a2151f939b31f3edd2c604dba3e2b0a8
vlad <daprostovseeto@gmail.com>
2021-07-26 19:31:18 +0300

    update project

--- redirect.py
+++ redirect.py
@@ -1,100 +1,95 @@
 #!/usr/bin/env python
-import requests,sys,time,os, argparse
-top = """
- #################################################
- # Open redirect Scanner for dummies like me :)  #
- # by: ak1t4 (know.0nix@gmail.com)               #
- #  twitter.com/knowledge_2014                   #
- #  hackerone.com/ak1t4                          #
- #  contributor(s): @sxcurity                    #
- #################################################
-    """
+import argparse
+import asyncio
+import json
+import os
+import signal
+from concurrent.futures import ThreadPoolExecutor
+from typing import List
 
-def main():
-    os.system('clear')
+import requests
+import urllib3
 
-    print(top)
+results: List[dict] = []
 
+
+def url_generator(domains_path: str, payload_path: str):
+    with open(domains_path) as d:
+        with open(payload_path) as p:
+            for domain in d:
+                for payload in p:
+                    yield f'https://{domain.strip()}{payload}'
+
+
+def test_open_redirect(s: requests.Session, url: str):
+    print(url)
+    try:
+        resp = s.get(url=url, timeout=10, verify=False)
+        if not resp.history:
+            return
+
+        result = {
+            'link': url,
+            'link_code': resp.status_code,
+            'data': []
+        }
+        for r in resp.history[:1]:
+            result['data'].append({
+                'result': 'Request was redirected',
+                'link': r.url,
+                'code': r.status_code
+            })
+        result['data'].append({
+            'result': 'Final destination',
+            'link': resp.url,
+            'code': resp.status_code
+        })
+        results.append(result)
+    except (requests.exceptions.RequestException, urllib3.exceptions.HTTPError):
+        pass
+
+
+def shutdown():
+    print('Graceful shutdown...')
+    for task in asyncio.all_tasks():
+        task.cancel()
+
+
+async def main():
     # Payloads example
     # This is replaced with a payloads.list (a lot of amazing redirect payloads)
-    #payload = '//www.google.com/%2F..'
-    #payload2 = '//www.yahoo.com//'
-    #payload3 = '//www.yahoo.com//%2F%2E%2E'
+    # payload = '//www.google.com/%2F..'
+    # payload2 = '//www.yahoo.com//'
+    # payload3 = '//www.yahoo.com//%2F%2E%2E'
 
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('-d', help='path to file of domain list', nargs=1, dest='domain', required=True)
-    parser.add_argument('-w', help='payload wordlist', nargs=1, dest='payload', required=True)
-
+    parser.add_argument('-d', help='path to file of domain list', dest='domain', required=True)
+    parser.add_argument('-w', help='payload wordlist', dest='payload', default='payloads.list')
+    parser.add_argument('-o', help='output filename, default output.json', dest='output', default='output.json')
     args = parser.parse_args()
+    loop = asyncio.get_event_loop()
+    futures = []
 
-    # first argument - file with subdomains
+    print('Searching the ex-girlfriend target & Holy Grail at [303 see others]...')
 
-    file = args.domain[0]
+    # open file with subdomains and iterates
+    # loop for find the trace of all requests (303 is an open redirect) see the final destination
+    with requests.Session() as s:
+        with ThreadPoolExecutor(max_workers=os.cpu_count() * 1) as pool:
+            for url in url_generator(domains_path=args.domain, payload_path=args.payload):
+                futures.append(loop.run_in_executor(pool, test_open_redirect, s, url))
 
-    # second argument - payload string
+            for signame in {'SIGINT', 'SIGTERM'}:
+                loop.add_signal_handler(getattr(signal, signame), shutdown)
 
-    payloads = args.payload[0]
+            await asyncio.gather(*futures)
 
-    #open file with subdomains and iterates
- 
-    with open(file) as f:
-
-		print ""
-		print "Searching the ex-girlfriend target &  Holy Grail at [303 see others].."
-		print ""
-		time.sleep(4)
+    with open(args.output, 'w') as f:
+        json.dump(results, f, indent=3)
 
 
-		# loop for find the trace of all requests (303 is an open redirect) see the final destination
-
-                for line in f:
-		    with open(payloads) as p:
-			for payload in p:
-                    	    try:
-
-                        	line2 = line.strip()
-
-                        	line3 = 'https://' + line2 + payload
-
-                        	print line3
-
-                        	response = requests.get(line3, verify=True)    
-
-                        	print response
-
-                        	try:
-
-                            	    if response.history:
-                             
-                                	print "Request was redirected"
-                             
-                                	for resp in response.history:
-
-                                    	    print "|"
-                                    	    print resp.status_code, resp.url
-                                    
-
-                                	print "Final destination:"
-
-                                	print "+"
-                                	print response.status_code, response.url
-
-                                
-                           	    else:
-
-                                	print "Request was not redirected"
-
-                            
-                        	except:
-                            	    print "connection error :("
-
-                   	    except:
-
-                        	print "quitting.."
-
-try:
-	main()
-except IndexError:
-	print(" Usage: python "+sys.argv[0]+" [subdomains.file] [redirect.payload]\n")
-        print(" Example python "+sys.argv[0]+" uber.list '//yahoo.com/%2F..'\n")
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except asyncio.exceptions.CancelledError:
+        pass
